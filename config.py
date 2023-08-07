e4536c6fccada717e2c09665bcd87b2b0416ac36
lirkblack <118726481+lirkblack@users.noreply.github.com>
2023-08-07 06:57:00 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+valid_phone = "79097929818"
+valid_pass = "PolLi!tra44"
+valid_email = "test_lirk@mail.ru"
+valid_login = "rtkid_1691236292112"
+valid_ls = "123456654321"
+invalid_phone = "79092222222"
+invalid_email = "test_lirk2@mail.ru"
